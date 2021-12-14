a811947b6f9728dc1d32ef9b741b0dcd66081e06
guscarpim <gustavoscarpim@gmail.com>
2021-12-14 00:29:57 -0300

    add: Themes pages

---
+++ myapp/src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
+
+import LandingPage from './Pages';
+
+import GlobalStyle from './GlobalStyles';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <LandingPage />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
