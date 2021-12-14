be30c767852b0680aa56431c1a4d01205a385f4d
guscarpim <gustavoscarpim@gmail.com>
2021-12-14 00:30:03 -0300

    add: themes

---
+++ myapp/src/theme.ts
@@ -0,0 +1,80 @@
+const theme = {
+  font: {
+    primary: 'Nunito, sans-serif'
+  },
+  color: {
+    primary: {
+      default: '#F63739',
+      secondary: '#FFD700',
+    },
+    white: '#FFFFFF',
+    gray: {
+      default: '#C4C4C4',
+      dark: '#666666',
+      darker: '#868A94'
+    },
+    black: '#333333'
+  },
+  size: {
+    xnano: '4px',
+    nano: '5px',
+    xxsmall: '10px',
+    xsmall: '12px',
+    small: '14px',
+    medium: '16px',
+    large: '24px',
+    xlarge: '36px',
+    xxlarge: '48px'
+  },
+  weight: {
+    light: 300,
+    regular: 400,
+    semiBold: 500,
+    bold: 700,
+    black: 900
+  },
+  lineHeight: {
+    xsmall: '12px',
+    small: '16px',
+    medium: '18px',
+    large: '26px'
+  },
+  border: {
+    radius: {
+      large: '10px',
+      medium: '8px',
+      small: '6px',
+      circle: '50%',
+      pill: '200px'
+    }
+  },
+  shadow: {
+    level: {
+      1: '0 0 6px',
+      2: '0 0 10px',
+      3: '0 0 16px',
+    },
+    color: 'rgba(0, 0, 0, 0.16)',
+  },
+  spacing: {
+    quark: '6px',
+    nano: '8px',
+    normal: '12px',
+    xxxsmall: '16px',
+    xxsmall: '24px',
+    xsmall: '32px'
+  },
+  zIndex: {
+    dropdown: "1000",
+    sticky: "1020",
+    fixed: "1030",
+    drawerBackdrop: "1040",
+    drawer: "1050",
+    modalBackdrop: "1060",
+    modal: "1070",
+    pooper: "1080",
+    tooltip: "1090"
+  }
+};
+
+export default theme
