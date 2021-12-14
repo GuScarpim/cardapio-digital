6d1b162937de0533060fcaa3df668e59a1b4a695
guscarpim <gustavoscarpim@gmail.com>
2021-12-14 00:30:36 -0300

    styles burger

---
+++ myapp/src/UI/molceules/Burger/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+interface INav {
+  open: boolean;
+}
+
+export const ContainerBurger = styled.div<INav>`
+  position: fixed;
+  top: ${({ theme }) => theme.spacing.xxxsmall};
+  right: ${({ theme }) => theme.spacing.xxsmall};
+  z-index: ${({ theme }) => theme.zIndex.drawer};
+  cursor: pointer;
+  display: none;
+
+  @media (max-width: 768px) {
+    display: flex;
+    justify-content: space-around;
+    flex-flow: column nowrap;
+  }
+`
+
+export const LineBurger = styled.div<INav>`
+    width: ${({ theme }) => theme.size.xlarge};
+    height: ${({ theme }) => theme.size.nano};
+    margin-bottom: ${({ theme }) => theme.spacing.quark};
+    background-color: ${({ open }) => open ? '#000' : '#000'};
+    border-radius: ${({ theme }) => theme.border.radius.large};
+    transform-origin: 1px;
+    transition: all 0.3s ease-in-out;
+    cursor: pointer;
+
+    &:nth-child(1) {
+      transform: ${({ open }) => open ? 'rotate(48deg)' : 'rotate(0)'};
+    }
+    &:nth-child(2) {
+      transform: ${({ open }) => open ? 'translateX(100%)' : 'translateX(0)'};
+      opacity: ${({ open }) => open ? 0 : 1};
+    }
+    &:nth-child(3) {
+      margin-top: ${({ open, theme }) => open && theme.size.xnano};
+      transform: ${({ open }) => open ? 'rotate(-48deg)' : 'rotate(0)'};
+    }
+`
