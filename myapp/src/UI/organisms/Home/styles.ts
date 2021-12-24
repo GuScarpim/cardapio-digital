b8ce561d1a7192d33a847c2a0542c072ca12799a
guscarpim <gustavoscarpim@gmail.com>
2021-12-24 19:04:33 -0300

    Home

---
+++ myapp/src/UI/organisms/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+import Background from '../../../Assets/home/home-bg.jpg'
+
+export const Container = styled.div`
+  background-image: url(${Background});
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  width: 100vw;
+  height: 600px;
+`
